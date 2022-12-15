baf272ef701c147624003b6fdf2ad7095c05367a
Александр Раковчен <italianhorse20@gmail.com>
2022-12-15 21:25:14 +0300

    adding done

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -9,14 +9,13 @@ describe('Credit Card Validator form', () => {
   let server = null;
   const baseUrl = 'http://localhost:8888';
 
-  beforeAll(async (done) => {
+  beforeAll(async () => {
     server = fork(`${__dirname}/e2e.server.js`);
     await new Promise((resolve, reject) => {
       server.on('error', reject);
       server.on('message', (message) => {
         if (message === 'ok') {
           resolve();
-          done();
         }
       });
     });
