1c0ad495ed49b677b6687551ae624b3d65836914
Александр Раковчен <italianhorse20@gmail.com>
2022-12-15 21:33:48 +0300

    adding done

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -28,10 +28,9 @@ describe('Credit Card Validator form', () => {
     page = await browser.newPage();
   });
 
-  afterAll(async (done) => {
+  afterAll(async () => {
     await browser.close();
     server.kill();
-    done();
   });
 
   test.each([
@@ -39,7 +38,7 @@ describe('Credit Card Validator form', () => {
     'afaasfk',
     '135463746354',
     '4400 4164 2568 1234',
-  ])('test form-control invalid', async (n) => {
+  ])('test form-control invalid', async (n, done) => {
     await page.goto(baseUrl);
     const formGroup = await page.$('.form-group');
     const input = await formGroup.$('.form-control');
@@ -51,6 +50,7 @@ describe('Credit Card Validator form', () => {
     const msg = await formGroup.$eval('.msg', (el) => el.classList.value);
 
     await expect(msg).toEqual('msg invalid');
+    done();
   });
 
   test.each([
@@ -61,7 +61,7 @@ describe('Credit Card Validator form', () => {
     ['5555555555554444', 'master'],
     ['4111111111111111', 'visa'],
     ['2200 0000 0000 0004', 'mir'],
-  ])('test form-control frame', async (n, res) => {
+  ])('test form-control frame', async (n, res, done) => {
     await page.goto(baseUrl);
     const formGroup = await page.$('.form-group');
     const input = await formGroup.$('.form-control');
@@ -73,5 +73,6 @@ describe('Credit Card Validator form', () => {
     const cardInFrame = await page.$eval('.frame', (el) => el.classList.value);
 
     await expect(cardInFrame).toEqual(`card ${res} frame`);
+    done();
   });
 });
