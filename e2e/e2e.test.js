741cefe6e4557a47c72a6eae99ddfbb5cc6e0813
Александр Раковчен <italianhorse20@gmail.com>
2022-12-15 22:45:09 +0300

    dirname with path

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,6 +1,8 @@
 import puppetteer from 'puppeteer';
 import { fork } from 'child_process';
 
+const path = require('path');
+
 jest.setTimeout(30000); // default puppeteer timeout
 
 describe('Credit Card Validator form', () => {
@@ -10,7 +12,7 @@ describe('Credit Card Validator form', () => {
   const baseUrl = 'http://localhost:8888';
 
   beforeAll(async () => {
-    server = fork(`${__dirname}/e2e.server.js`);
+    server = fork(`${path.resolve(__dirname)}/e2e.server.js`);
     await new Promise((resolve, reject) => {
       server.on('error', reject);
       server.on('message', (message) => {
